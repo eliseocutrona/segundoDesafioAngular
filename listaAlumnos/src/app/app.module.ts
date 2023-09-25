0a7c64f3a5a702dcfdd6ee302785c826e23846f3
eliseocutrona <eliseocutrona@gmail.com>
2023-09-25 20:42:35 -0300

    segundoDesafio

--- listaAlumnos/src/app/app.module.ts
+++ listaAlumnos/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DirectivasComponent } from './components/directivas/directivas.component';
+import { DirectivaPersonalizadaDirective } from './directives/directiva-personalizada.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DirectivasComponent,
+    DirectivaPersonalizadaDirective
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
